2a1255190dce0caa397a6ebe8480b52704d51ba8
FARUK2059 <omarfaruk34552@gmail.com>
2025-02-17 12:19:48 +0600

    laut setup

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+
+
+
+const Login = () => {
+
+    return (
+        <div>
+            <div> The Component is Start Login  </div>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/app/register/page.tsx
@@ -0,0 +1,13 @@
+
+
+
+const Registation = () => {
+
+    return (
+        <div>
+            <div> The Component is Start Registation </div>
+        </div>
+    );
+};
+
+export default Registation;
